da90bdb80520793b3be5758f9ca2a5c936a10aaa
MrArsalAli <159891871+MrArsalAli@users.noreply.github.com>
2024-12-31 23:34:49 +0500

    2025

---
+++ typescript demo.ts
@@ -0,0 +1,18 @@
+//Data Types
+
+let userName : string = "Arsalan";
+let userAge : number = 22;
+let islearning : boolean = true;
+let colors : Array<string | number> = ["Yellow", "Red", 1, 2, 3]
+
+interface Test {
+    name : string;
+    age : number;
+    islearning : boolean;
+}
+
+let test: Test ={
+    name : "Arsalan",
+    age : 22,
+    islearning : true
+};
